f0e93c20aa35a1aa61bba5264395e270bc510f01
luis Ortega <ortegaluiskb@gmail.com>
2026-05-19 17:54:12 -0600

    running

--- src/python/11._coding.py
+++ src/python/11._coding.py
@@ -5,7 +5,7 @@ from root import Pragmatic
 class Ortogonalidad(Pragmatic, ABC):
 
     def __init__(self) -> None:
-        pass
+        print('Aplicar los consejos de Ortogonalidad')
 
     @abstractmethod
     def query(cls) -> None:
@@ -17,12 +17,57 @@ class Ortogonalidad(Pragmatic, ABC):
 
     @classmethod
     def reboot(cls) -> None:
-        raise NotImplementedError
+        print('Reinicio del metodo abstracto')
 
     @abstractmethod
     def recovery(cls) -> None:
-        raise NotImplementedError
+        print('Recuperación del metodo abstracto')
 
     @abstractmethod
     def shutdown(cls) -> None:
-        raise NotImplementedError
+        print('Apagar el metodo abstracto')
+
+class Running(Ortogonalidad):
+    
+    __conteiner: str = 'docker'
+    count: int = 0
+    def __init__(self) -> None:
+        super().__init__()
+        print('En ejecucion el objeto con sus herencias')
+
+    @staticmethod
+    def counter() -> int:
+        Running.count += 1
+        return Running.count
+    
+    @property
+    def conteiner(self) -> str:
+        return self.__conteiner
+    
+    @conteiner.setter
+    def conteiner(self, conteiner: str) -> None:
+        self.__conteiner = conteiner
+
+    @abstractmethod
+    def query(cls) -> None:
+        pass
+
+    @classmethod
+    def reboot(cls) -> None:
+        print('Reinicio del metodo abstracto')
+
+    @abstractmethod
+    def recovery(cls) -> None:
+        print('Recuperación del metodo abstracto')
+
+    @abstractmethod
+    def shutdown(cls) -> None:
+        print('Apagar el metodo abstracto')
+
+# <-- main -->
+if __name__ == "__main__":
+
+    Running.counter()
+    print(Running.counter())
+
+    # Object
