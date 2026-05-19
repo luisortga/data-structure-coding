e412e436dd7c56dd1ae6b0c8ffb929cc32a4a9da
luis Ortega <ortegaluiskb@gmail.com>
2026-05-19 17:19:04 -0600

    metodo abstracto

--- src/python/11._coding.py
+++ src/python/11._coding.py
@@ -1,6 +1,15 @@
 # start
+from abc import ABC, abstractmethod
 
-class Ortogonalidad:
+class Ortogonalidad(ABC):
 
     def __init__(self) -> None:
         pass
+
+    @abstractmethod
+    def query(self) -> None:
+        pass
+
+    @staticmethod
+    def build(count: int) -> int:
+        return 0 # pass
