6e6b14f0f9f803b98016854a9bdd28fc7dcfa590
luis Ortega <ortegaluiskb@gmail.com>
2026-05-21 16:58:06 -0600

    balaz trazadoras js

---
+++ src/js/12._balas_trazadoras.js
@@ -0,0 +1,18 @@
+// Balas trazadoras, inspiración en el libro el programador pragmatico
+
+class Trazo {
+
+    /** @type {string} */
+    #trace = ""
+
+    constructor(way, map, count) {
+        /** @type {string} */
+        this.way = way
+        /** @type {string} */
+        this.map = map
+        /** @type {number} */
+        this.count = count
+    }
+
+
+}
