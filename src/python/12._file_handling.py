fe7ba1561bdc213c0eafa1d6bacbc7d63b9da35c
luis Ortega <ortegaluiskb@gmail.com>
2026-05-25 19:59:10 -0600

    manejo de ficheros mejorado

--- src/python/12._file_handling.py
+++ src/python/12._file_handling.py
@@ -1,21 +1,23 @@
-# File Handling en Python
+# correction
 
-# Open a file for reading and writing
 try:
     with open('polimorfismo/archivo.txt', 'r+') as file:
-    # file.write('Invincible\n')
-        file.seek(0)  # Move the file pointer to the beginning
+        file.seek(0)
         print(file.read())
+
 except FileNotFoundError as e:
     print(f"The file was not found. Error: {e}")
 
-    
+
 try:
-    with open('polimorfismo/skills.js', 'r+') as file:
-        file.write('console.log("Hello, World!")\n')
-        file.write('let edad = 25;// Esto es correcto\nedad = 27\n')
-        file.write('console.log(`mi edad es: ${edad}`)\n')
+    with open('polimorfismo/skills.js', 'w+') as file:
+        file.write('console.log("Hello, World!");\n')
+        file.write('let edad = 25;\n')
+        file.write('edad = 27;\n')
+        file.write('console.log(`Mi edad es: ${edad}`);\n')
+
         file.seek(0)
         print(file.read())
+
 except FileNotFoundError as e:
     print(f"The file was not found. Error: {e}")
