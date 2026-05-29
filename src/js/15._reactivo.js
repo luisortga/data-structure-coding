b8b4642b7e476c92b310929fae8cbcc0b953485a
luis Ortega <ortegaluiskb@gmail.com>
2026-05-28 21:44:24 -0600

    actualizar rxjs en js

--- src/js/15._reactivo.js
+++ src/js/15._reactivo.js
@@ -5,3 +5,12 @@ import { mergeMap } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax'
 import { logValues } from '../rxcommon/logger.js'
 
+let users = Observable.of(3, 2, 1)
+let result = users.pipe(
+mergeMap((user) =>
+ajax.getJSON(`https://reqres.in/api/users/${user}`))
+)
+result.subscribe(
+resp => logValues(JSON.stringify(resp.data)),
+err => console.error(JSON.stringify(err))
+)
