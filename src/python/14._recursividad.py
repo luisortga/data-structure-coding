3c38ce3c3eebc776a9473b01b20ad10dce231f42
luis Ortega <ortegaluiskb@gmail.com>
2026-05-31 12:08:55 -0600

    agregando recursividad py

--- src/python/14._recursividad.py
+++ src/python/14._recursividad.py
@@ -1,1 +1,12 @@
 # pending
+# import asyncio
+
+def recursividad(n: int) -> int:
+    if n <= 1:
+        return 1
+    else:
+        return n * recursividad(n - 1)
+
+if __name__ == "__main__":
+    print('Recursividad.')
+    print(recursividad(6))
