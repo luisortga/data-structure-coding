0f31db7e043c061cecb6bc1e22bb7448c344aa9e
luis Ortega <ortegaluiskb@gmail.com>
2026-05-31 12:08:55 -0600

    Change version

--- src/python/13._recursividad.py
+++ src/python/13._recursividad.py
@@ -1,12 +1,2 @@
-#
-import re
+# 
 
-text = "Los frenos se cristalizaron, corres la final mañana de la cademia GT, Gran Turismo, Sony"
-
-pattern: str = r'cristalizaron'
-find = re.search(pattern, text)
-
-if find:
-    print(find.group())
-else:
-    print('resultado None')
