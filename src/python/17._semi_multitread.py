0334df23e09228634b6a37006dff771fa0666241
luis Ortega <ortegaluiskb@gmail.com>
2026-06-01 11:14:53 -0600

    Update semil multi thread

--- src/python/17._semi_multitread.py
+++ src/python/17._semi_multitread.py
@@ -2,20 +2,27 @@
 import threading
 import time
 
-def inflar_globos():
+def inflar_globos() -> None:
     for i in range(3):
         print(f'Billy esta inflando el globo {i}')
         time.sleep(4)
         print('Billy termino de inflar los globos')
 
-def hornear_pie():
+def hornear_pie() -> None:
     for _ in range(4):
         print("Mandy esta horneando el pastel...")
         time.sleep(4)
         print('Mandy termino de hornear el pastel')
 
+def reproducir_musica() -> None:
+    for i in range(0, 8):
+        print('colocando playlist')
+        time.sleep(4)
+        print(f'Se reproduce la cancion {i+1}')
+
 thread_billy = threading.Thread(target=inflar_globos)
 thread_mandy = threading.Thread(target=hornear_pie)
+thread_musica = threading.Thread(target=reproducir_musica)
 
 # bloqueado por GIL
 
@@ -23,8 +30,10 @@ thread_mandy = threading.Thread(target=hornear_pie)
 thread_billy.start()
 # Ejecutar en otro semi thread
 thread_mandy.start()
+thread_musica.start()
 
 thread_billy.join()
 thread_mandy.join()
+thread_musica.join()
 
 print('La fiesta esta lista.')
