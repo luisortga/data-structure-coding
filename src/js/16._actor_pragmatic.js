78e9df0ac67e07f486ad9e2b4406c4e32041cd7d
luis Ortega <ortegaluiskb@gmail.com>
2026-06-01 19:42:21 -0600

    Update actor pragmatic js

---
+++ src/js/16._actor_pragmatic.js
@@ -0,0 +1,74 @@
+//
+
+const cusomerActor = {
+    'hungry for pie': (msg, ctx, state) => {
+        return dispatch(state.waiter, { type: "order", customer: ctx.self, wants: 'pie' })
+    },
+    'put on table' : (msg, ctx, _state) => {
+        console.log(`${ctx.self.name} sees "${msg.food}" aparecer en la mesa`)
+    },
+    'no pie left': (_msg, ctx, _state) => 
+        console.log(`${ctx.self.name} sulk...`)
+}
+
+const waiterActor = {
+    "order": (msg, ctx, state) => {
+        if (msg.wants === "pie") {
+            dispatch(state.pieCase, {
+                type: "get slice", costumer: msg.customer, waiter: ctx.self })
+        } else  {
+            console.dir(`Dont know to order ${msg.wants}`)
+        }
+    },
+    "add to order": (msg, ctx) =>
+        console.log(`Waiter add ${msg.food} to ${msg.customer.name}'s order`),
+    "error": (msg, ctx) => {
+        dispatch(msg.customer, { type: 'no pie left', msg: msg.msg })
+        console.log(`\nThe waiter apologizes to ${msg.customer.name}: ${msg.msg}`)
+    }
+}
+
+const pieCaseActor = {
+    'get slice': (msg, context, state) => {
+        if (state.slice.length === 0) {
+            dispatch(msg.waiter, {  type: 'error', msg: "no pie left", customer: msg.customer   })
+            return state
+        } else {
+            let slice = state.slices.shift() + ' pie slice'
+            dispatch(msg.customer, {
+                type: 'put on table', food: slice   })
+            dispatch(msg.waiter, {
+                type: 'add to order', food: slice, customer: msg.customer   })
+                return state
+        }
+    }
+}
+
+const actorSystem = start();
+
+let pieCase = start_actor(
+    actorSystem,
+    'pie-case',
+    pieCaseActor,   {   slices: ["apple", "peach", "cherry"] })
+
+let waiter = start_actor (
+    actorSystem,
+    'waiter',
+    waiterActor,
+    { pieCase: pieCase })
+
+let c1 = start_actor(actorSystem, 'customer1',
+    customerActor, { waiter: waiter })
+
+let c2 = start_actor(actorSystem, 'customer2',
+    customerActor, { waiter: waiter })
+
+dispatch(c1, {type: 'hungry for pie', waiter: waiter    })
+dispatch(c2, {type: 'hungry for pie', waiter: waiter    })
+dispatch(c1, {type: 'hungry for pie', waiter: waiter    })
+dispatch(c2, {type: 'hungry for pie', waiter: waiter    })
+dispatch(c1, {type: 'hungry for pie', waiter: waiter    })
+sleep(500)
+    .then(() => {
+        stop(actorSystem)
+    })
