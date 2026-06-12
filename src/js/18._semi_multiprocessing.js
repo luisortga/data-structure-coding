82feed405780f5f0e1ebe87624764bfaa5e6172c
luis Ortega <ortegaluiskb@gmail.com>
2026-06-11 23:18:38 -0600

    Update multi processing

--- src/js/18._semi_multiprocessing.js
+++ src/js/18._semi_multiprocessing.js
@@ -1,1 +1,16 @@
 //
+
+const control = () => {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                r1: '',
+                r2: 'shooter',
+                l1: '',
+                l2: 'apuntar',
+                rectangle: 'change guns'
+            })
+        })
+    })
+}
