92bbd1ad27ea17bdb2e11f0e9c07b69c0b1fe127
luis Ortega <ortegaluiskb@gmail.com>
2026-06-11 23:25:56 -0600

    Upgrade processing with except

--- src/js/18._semi_multiprocessing.js
+++ src/js/18._semi_multiprocessing.js
@@ -12,6 +12,21 @@ const control = () => {
                 rectangle: 'change guns',
                 circle: 'recharge'
             })
-        })
+        }, 6000)
     })
 }
+
+const threading = async () => {
+
+    try {
+
+        console.log('start...')
+        let ps5 = await control()
+        print(ps5)
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+threading()
