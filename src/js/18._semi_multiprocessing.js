581e415ffacbfc1895f0e5fd2c28459c9faa6ff1
luis Ortega <ortegaluiskb@gmail.com>
2026-06-11 23:22:14 -0600

    Update object successful

--- src/js/18._semi_multiprocessing.js
+++ src/js/18._semi_multiprocessing.js
@@ -5,11 +5,12 @@ const control = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve({
-                r1: '',
+                r1: 'launch tactical equipment',
                 r2: 'shooter',
-                l1: '',
-                l2: 'apuntar',
-                rectangle: 'change guns'
+                l1: 'launch lethal equipment',
+                l2: 'aim',
+                rectangle: 'change guns',
+                circle: 'recharge'
             })
         })
     })
