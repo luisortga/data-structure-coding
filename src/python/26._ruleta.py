3cde4a6d0611ffbdaedd71c1bce6a1818933b945
luis Ortega <ortegaluiskb@gmail.com>
2026-06-14 23:14:50 -0600

    Update ruleta py

---
+++ src/python/26._ruleta.py
@@ -0,0 +1,12 @@
+import random
+
+ruleta = list(range(37))+[7]*10
+
+count7: int = 0
+for i in range(1000):
+    
+    if random.choice(ruleta) == 7:
+        count7 += 1
+        
+print(count7)
+    
