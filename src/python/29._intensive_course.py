6e35bb8a733088f4d2eaa074d555c52375399d19
luis Ortega <ortegaluiskb@gmail.com>
2026-06-16 20:49:38 -0600

    Update intensive and datetime

--- src/python/29._intensive_course.py
+++ src/python/29._intensive_course.py
@@ -1,4 +1,5 @@
 # Curso intensivo de python
+from datetime import datetime
 
 MAX_COUNT: int = 10_000
 
@@ -9,8 +10,19 @@ author: str = 'J. Robert Oppenheimer'
 
 print(f'{author} : >> {phrase}')
 
-day: str = 'lunes'
+ahora = datetime.now()
+day_week: int = ahora.weekday()
 
-match day:
-    case 'lunes':
-        print('')
+day: str = ''
+
+match day_week:
+    case 0:
+        day = 'lunes'
+    case 1:
+        day = 'martes'
+    case 2:
+        day = 'miercoles'
+    case 3:
+        day = 'jueves'
+        
+print(f'El dia es: {day}')
