1e5d8821ed55ede2cb2322c4acad1dd8fefbe2fb
luis Ortega <ortegaluiskb@gmail.com>
2026-06-16 20:33:28 -0600

    Create intensive of book py

---
+++ src/python/29._intensive_course.py
@@ -0,0 +1,10 @@
+# Curso intensivo de python
+
+MAX_COUNT: int = 10_000
+
+print('Hello World', MAX_COUNT, '\n', end=" ")
+
+phrase: str = 'Ahora me he convertido en la Muerte, el destructor de mundos'
+author: str = 'J. Robert Oppenheimer'
+
+print(f'{author} : >> {phrase}')
