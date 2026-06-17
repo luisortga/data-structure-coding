cb4dd408060e201fd02bc62384d65ab2756bf056
luis Ortega <ortegaluiskb@gmail.com>
2026-06-16 20:53:37 -0600

    Update datetime with match

--- src/python/29._intensive_course.py
+++ src/python/29._intensive_course.py
@@ -15,7 +15,7 @@ day_week: int = ahora.weekday()
 
 day: str = ''
 
-match day_week:
+match day_week: # type: ignore
     case 0:
         day = 'lunes'
     case 1:
@@ -24,5 +24,13 @@ match day_week:
         day = 'miercoles'
     case 3:
         day = 'jueves'
+    case 4:
+        day = 'viernes'
+    case 5:
+        day = 'sabado'
+    case 6:
+        day = 'domingo'
+    case _:
+        day = 'final'
         
 print(f'El dia es: {day}')
