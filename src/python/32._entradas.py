3abca0783acc06f94f79bc6f76d0eb50c4e7ea59
luisOrtga <ortegaluiskb@gmail.com>
2026-06-22 22:09:19 -0600

    Upgrade input py

--- src/python/32._entradas.py
+++ src/python/32._entradas.py
@@ -3,5 +3,7 @@ numero: str | int = input('Ingrese un numero y te dire si es par o inpar:\n')
 numero = int(numero)
 if numero % 2 == 0:
     print(f'El numero {numero} es par')
+elif numero == 24:
+    print('kobe bryant')
 else:
     print(f'El numero {numero} es inpar')
