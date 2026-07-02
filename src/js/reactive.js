47982a209ad468ba6fd0e97dbe4ad54fef93d351
luisOrtga <ortegaluiskb@gmail.com>
2026-07-02 11:22:23 -0600

    Upgrade reactive and observable

--- src/js/reactive.js
+++ src/js/reactive.js
@@ -1,11 +1,22 @@
-import { interval } from 'rxjs'
+import { interval, Observable } from 'rxjs'
 import { map, filter } from 'rxjs/operators'
 
 const contador = interval(1000)
 
+/*
 contador
   .pipe(
     map(valor => valor * 2),
     filter(valor => valor % 4 === 0)
   )
-  .subscribe(valor => console.log('Valor recibido:', valor))
+  .subscribe(valor => console.log('Valor recibido:', valor)) */
+
+const cold$ = new Observable((observer) => {
+  observer.next(Math.round(Math.random() * 10))
+})
+
+cold.subscribe((res) => {
+  console.log(res)
+})
+
+
