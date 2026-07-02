9a251eb78ed846fa9b05989a789dff52972b09c9
luisOrtga <ortegaluiskb@gmail.com>
2026-07-01 22:31:04 -0600

    Upgrade reactive js

--- src/js/reactive.js
+++ src/js/reactive.js
@@ -1,11 +1,11 @@
-import { interval } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { interval } from 'rxjs'
+import { map, filter } from 'rxjs/operators'
 
-const contador$ = interval(1000);
+const contador = interval(1000)
 
-contador$
+contador
   .pipe(
     map(valor => valor * 2),
     filter(valor => valor % 4 === 0)
   )
-  .subscribe(valor => console.log('Valor recibido:', valor));
+  .subscribe(valor => console.log('Valor recibido:', valor))
