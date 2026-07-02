989729890d8d1c7cbfcea9d8510ef75182a1f460
luisOrtga <ortegaluiskb@gmail.com>
2026-07-02 11:34:22 -0600

    Upgrade reactive and resolved

--- src/js/reactive.js
+++ src/js/reactive.js
@@ -1,4 +1,4 @@
-import { interval, Observable } from 'rxjs'
+import { interval, Observable, Subject } from 'rxjs'
 import { map, filter } from 'rxjs/operators'
 
 const contador = interval(1000)
@@ -11,6 +11,8 @@ contador
   )
   .subscribe(valor => console.log('Valor recibido:', valor)) */
 
+// cold - ice cream
+
 const cold$ = new Observable((observer) => {
   observer.next(Math.round(Math.random() * 10))
 })
@@ -19,4 +21,16 @@ cold.subscribe((res) => {
   console.log(res)
 })
 
+// hot fix
 
+const hot$ = new Subject()
+
+hot$.subscribe(() => {
+  console.log(res)
+})
+
+hot$.subscribe(() => {
+  console.log(res)
+})
+
+hot$.next(Math.round(Math.random() * 10))
