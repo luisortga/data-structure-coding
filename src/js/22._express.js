b03ca33ef17deeebd553f55c43ccacdc40ee8d6e
luisOrtga <ortegaluiskb@gmail.com>
2026-07-09 20:38:58 -0600

    Update express node with commentary middleware

--- src/js/22._express.js
+++ src/js/22._express.js
@@ -4,28 +4,40 @@ const ditto = require('./pokemon/ditto.json')
 const app = express()
 app.disable('x-powered-by')
 
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.header['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', (chunk) => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la informacion en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
+
 const PORT = process.env.PORT ?? 1234
 
-app.get('/', (req, res) => {
+app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-
-  // escuchar el evento data
-  req.on('data', (chunk) => {
-    body += chunk.toString()
-  })
-
-  req.on('end', () => {
-    const data = JSON.parse(body)
-
-    data.timestamp = Date.now()
-    res.status(201).json(data)
-  })
+  res.status(201).json(req.body)
 })
 
+// la ultima que va a llegar
 app.use((req, res) => {
   res.status(404).send('<h1>404</h1>')
 })
