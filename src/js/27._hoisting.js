575d52a9c9bbd5fb9d2b020a0e7b4594699d29fd
luisOrtga <ortegaluiskb@gmail.com>
2026-07-23 18:22:52 -0600

    update with scope hoisting in js

--- src/js/27._hoisting.js
+++ src/js/27._hoisting.js
@@ -2,6 +2,7 @@
 // No utilizar var en javascript moderno
 // Hoisting
 
+// Temporal deadzone, valor indefinido, undefine
 console.log(team)
 console.log(squad)
 var team = 'BBVB'
@@ -16,3 +17,20 @@ console.log(squad)
 
 var squad = 'nuevo cambio'
 console.log(squad)
+
+//
+
+let greet = 'saludo global'
+
+class Snoopy {
+  constructor() {}
+
+  greeting() {
+    // let greet = 'saludo local'
+    console.log(greet)
+  }
+}
+
+// main
+const snow = new Snoopy()
+snow.greeting()
