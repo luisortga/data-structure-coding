be1c9fa331774ff4d8d958dd0648a0f08c49653b
luisOrtga <ortegaluiskb@gmail.com>
2026-07-23 20:29:28 -0600

    Update private property in js

--- src/js/28._propiedades_privadas.js
+++ src/js/28._propiedades_privadas.js
@@ -1,16 +1,23 @@
 class Cuenta {
   #saldo
+  #info
 
-  constructor(saldo) {
+  constructor(saldo, complemento) {
     this.#saldo = saldo
+    this.#info = `codex - ${complemento}`
   }
 
   comparar(otraCuenta) {
     return this.#saldo > otraCuenta.#saldo
   }
+
+  greeting(otraCuenta) {
+    return `${this.#info} and ${otraCuenta.#info} from best duple`
+  }
 }
 
-const a = new Cuenta(500)
-const b = new Cuenta(800)
+const a = new Cuenta(500, 'remi')
+const b = new Cuenta(800, 'loki')
 
 console.log(a.comparar(b))
+console.log(a.greeting(b))
