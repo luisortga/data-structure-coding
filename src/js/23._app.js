13a53f5d5b809564d45bbfcfa489066233296037
luisOrtga <ortegaluiskb@gmail.com>
2026-07-26 22:51:00 -0600

    Update app node more module

--- src/js/23._app.js
+++ src/js/23._app.js
@@ -2,13 +2,28 @@ const express = require('express')
 const crypto = require('node:crypto')
 const movies = require('./movies.json')
 const z = require('zod')
+const { validateMovie, validatePartialMovie } = require('./Schemas/movies')
 
 const app = express()
 app.use(express.json())
 app.disable('x-powered-by') // desabilitar
 
+const acceptedOrigins = [
+  'http://localhost:8080',
+  'http://localhost:8081',
+  'http://localhost:1234',
+  'http://movies.com'
+]
+
 // Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  // cuando la peticion es del mismo ORIGIN
+  // http://localhost:1234 -> http://localhost:1234
+  if (acceptedOrigins.includes(origin)) {
+  res.header('Access-Control-Allow-Origin', '*') // todos los origenes estan permitidos
+  }
+
   const { genre } = req.query
   if (genre) {
     //
@@ -23,50 +38,27 @@ app.get('/movies', (req, res) => {
 app.get('/movies/:id', (req, res) => {
   // path to regex
   const { id } = req.params
-  const movie = movies.find((movie) => movie.id === id)
+  const movie = movies.find((movies) => movies.id === id)
   if (movie) return res.json(movie)
 
   res.status(404).json({ message: 'Movie not found' })
 })
 
 app.post('/movies', (req, res) => {
-  const movieSchema = z.object({
-    title: z.string({
-      invalid_type_error: 'Movie title must be a string.',
-      required_error: 'Movie title is required.',
-    }),
-    year: z.number().int().min(1900).max(2027),
-    director: z.string(),
-    duration: z.number().int().positive(),
-    rate: z.number().min(0).max(10),
-    poster: z.string().url({
-      message: 'Poster must be a valid url'
-    }), //.endsWith('jpg')
-    genre: z.array(
-      z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi'])
-    )
-  })
-  const {
-    title,
-    genre,
-    year,
-    director,
-    duration,
-    rate,
-    poster
-  } = req.body
 
-  // security
+  // validate
+  
+  const result = validateMovie(req, req.body)
+  
+  if (result.error) {
+    // 422 specific
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
 
+  // en base de datos
   const newMovie = {
     id: crypto.randomUUID(), // uuid v4
-    title,
-    genre,
-    year,
-    director,
-    duration,
-    rate: rate ?? 0,
-    poster
+    ...result.data
   }
 
   // Esto no sería REST, porque estamos guardando
@@ -75,6 +67,26 @@ app.post('/movies', (req, res) => {
   movies.push(newMovie) // actualizar la caché del cliente
 
   res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.nessage) })
+  }
+  
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+     return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
 })
 
 const PORT = process.env.PORT ?? 1234
