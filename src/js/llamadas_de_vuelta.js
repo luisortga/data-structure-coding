b695d8800f361fe5436b4793867db1978bc44ba1
luisOrtga <ortegaluiskb@gmail.com>
2026-07-28 23:40:44 -0600

    Update File Callback

---
+++ src/js/llamadas_de_vuelta.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises; // Versión con promesas
+
+app.get('/usuario/:id', function(req, res) {
+  Promise.all([
+    fs.readFile('usuarios.json', 'utf8'),
+    fs.readFile('historial.json', 'utf8'),
+    fs.readFile('prefs.json', 'utf8')
+  ])
+  .then(([usuarios, historial, prefs]) => {
+    res.json({ usuarios, historial, prefs });
+  })
+  .catch(err => {
+    res.status(500).send("Error");
+  });
+});
