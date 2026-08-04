b40cd05dc948bf057885246ba54847e8eeb17941
luisOrtga <ortegaluiskb@gmail.com>
2026-08-03 23:57:58 -0600

    Add coded require js

--- src/js/33._requerido.js
+++ src/js/33._requerido.js
@@ -1,1 +1,11 @@
 /* testing from sale developer */
+
+let espera = setTimeout((res) => {
+    res(console.log('Sucessful'))
+}, 4000)
+
+const dataCompany = async () => {
+    return new Promise((res) => {
+
+    })
+}
