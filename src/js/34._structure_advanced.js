86d3f0da8415b2b245aa470c417784365db42261
luisOrtga <ortegaluiskb@gmail.com>
2026-08-09 22:34:53 -0600

    update structure with foreach

---
+++ src/js/34._structure_advanced.js
@@ -0,0 +1,10 @@
+// Structure advanced
+
+// Arrays avanzados - methods
+
+let cars = ["beantley", "aston martin", "mini", "mc laren", "rolls roys"]
+
+console.log('Reino Unido')
+cars.forEach(element => {
+    console.log(element)  
+})
