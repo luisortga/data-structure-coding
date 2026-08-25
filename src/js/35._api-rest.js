de0f50fd832898de91d346bb172069b00235c196
luisOrtga <ortegaluiskb@gmail.com>
2026-08-24 23:14:01 -0600

    Update api OpenWeather

--- src/js/35._api-rest.js
+++ src/js/35._api-rest.js
@@ -104,3 +104,21 @@ async function partialPostUpdate() {
 }
 
 partialPostUpdate()
+
+//
+
+async function getWeather(city) {
+    
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+    try {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
+getWeather("London")
